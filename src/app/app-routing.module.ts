e5d50e1159afb7e4562b74d30729ea5f41f45200
PKaiser <dev.pkaiser@gmail.com>
2020-05-10 16:29:01 +0200

    add routing to angular app and integrate shaker

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { EntryComponent } from './entry.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { ShakerComponent } from './shaker/shaker.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'entry',
+        component: EntryComponent,
+    },
+    {
+        path: 'shaker',
+        component: ShakerComponent,
+    },
+    {
+        path: '**',
+        redirectTo: 'entry',
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { EntryComponent } from './entry.component';
+import { NgModule } from '@angular/core';
+import { ShakerModule } from './shaker/shaker.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent, 
+        EntryComponent
+    ],
+    imports: [
+        BrowserModule, 
+        AppRoutingModule, 
+        ShakerModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
