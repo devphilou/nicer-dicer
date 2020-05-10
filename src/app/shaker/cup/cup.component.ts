4371bb86d3737fedd3242bbef7fe619ced624b0e
PKaiser <dev.pkaiser@gmail.com>
2020-05-10 16:24:44 +0200

    move cup component

--- src/app/shaker/cup/cup.component.ts
+++ src/app/shaker/cup/cup.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-cup',
-    templateUrl: 'cup.component.html',
+    templateUrl: './cup.component.html',
     styleUrls: ['./cup.component.scss'],
 })
 export class CupComponent implements OnInit {
